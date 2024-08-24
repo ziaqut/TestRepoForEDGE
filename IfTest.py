4bbf140243d4d6e14eedc96a5fd157c3976c7b5a
ziaqut <154807487+ziaqut@users.noreply.github.com>
2024-08-24 23:11:23 +1000

    done

---
+++ IfTest.py
@@ -0,0 +1,21 @@
+
+Open In Colab
+Testing conditional statment
+
+Here I have tried how a simple if-else works on Python.
+
+
+x = int(input("Enter an interger"))
+
+if x < 1:
+  x = 0
+  print("negative change to zero")
+elif x == 0:
+  print("Zero")
+elif x ==1:
+  print("Single")
+else:
+  print("More")
+     
+Enter an interger45
+More
